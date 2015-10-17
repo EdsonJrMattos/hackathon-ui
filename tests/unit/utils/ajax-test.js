a37fd3c1720e7cfc2f0b29c73203e3219401e998
Edson Junior Teles de Mattos <edsonjuniortdeveloper@gmail.com>
2015-10-17 15:53:33 -0300

    creates utils the ajax for CRUD

---
+++ tests/unit/utils/ajax-test.js
@@ -0,0 +1,10 @@
+import ajax from '../../../utils/ajax';
+import { module, test } from 'qunit';
+
+module('ajax');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = ajax();
+  assert.ok(result);
+});
