1cb25ed28b49ab42989673edd3c8f78640fa1cb5
Edson Junior Teles de Mattos <edsonjuniortdeveloper@gmail.com>
2015-10-18 06:56:48 -0200

    adds find

--- app/pods/jobs/new/route.js
+++ app/pods/jobs/new/route.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function  () {
-    return this.store.createRecord('vacancy');
+    return Ember.RSVP.hash({
+      vacancy: this.store.createRecord('vacancy'),
+      agency:   this.store.find('agency')
+    });
   }
 });
