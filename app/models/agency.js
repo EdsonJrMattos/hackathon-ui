68adcf7302254e38b286a5477eb82a5b4acf2b32
Edson Junior Teles de Mattos <edsonjuniortdeveloper@gmail.com>
2015-10-18 01:04:05 -0200

    adds DS for attributes

--- app/models/agency.js
+++ app/models/agency.js
@@ -5,7 +5,7 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   cnpj:  DS.attr('string'),
   phone: DS.attr('string'),
-  companyName: attr('string'),
+  companyName: DS.attr('string'),
   candidates: DS.hasMany('candidate', {async: true}),
   employers: DS.hasMany('employer', {async: true}),
   employerPending: DS.hasMany('employer', {async: true}),
