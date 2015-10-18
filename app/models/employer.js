7f3995c50664084b9a5a9c3548ac11e94d26d078
Edson Junior Teles de Mattos <edsonjuniortdeveloper@gmail.com>
2015-10-18 02:50:04 -0200

    changes attributes

--- app/models/employer.js
+++ app/models/employer.js
@@ -3,8 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name:     DS.attr('string'),
   email:    DS.attr('string'),
-  cpf:      DS.attr('string'),
-  rg:       DS.attr('string'),
+  cnpj:      DS.attr('string'),
   phone:    DS.attr('string'),
   city:   DS.attr('string'),
   neighborhood:   DS.attr('string'),
