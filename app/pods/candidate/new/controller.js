68e760305679a3244d8a065ad7c63a72bc11fcde
Edson Junior Teles de Mattos <edsonjuniortdeveloper@gmail.com>
2015-10-18 07:00:53 -0200

    code save for candidate

--- app/pods/candidate/new/controller.js
+++ app/pods/candidate/new/controller.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  agency:[{id:1,name:"banana"}],
   actions: {
       save:function () {
-        this.get('model')
-            .save();
+        this.get('model.candidate')
+            .save()
+            .then(this.transitionToRoute('candidate.show'));
+      },
+      Addcompetence:function  () {
+        var model = this.get('model.candidate');
+        model.get('competences')
+             .pushObject(this.store.createRecord('competence',{
+               name:this.get('competence')
+             }));
+        this.set('competence','');
       }
   }
 });
